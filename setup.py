c6437f2264c51351a90e3a412737999d0273eda7
K.A. Oman <koman@astro.rug.nl>
2017-10-16 10:10:45 +0200

    Initial barebones python package.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='martini',
+    version='0.1',
+    description='Synthetic datacube creation from simulations.',
+    url='',
+    author='Kyle Oman',
+    author_email='koman@astro.rug.nl',
+    license='',
+    packages=['martini'],
+    install_requires=['numpy', 'astropy'],
+    include_package_data=True,
+    zip_safe=False
+)
