73b7a196d8b4dd2584d1e88090dfc702580ef52a
Kyle Oman <koman@astro.rug.nl>
2017-11-02 08:22:53 -0700

    Typo fix.

--- martini/examples/example.py
+++ martini/examples/example.py
@@ -14,7 +14,7 @@ SO_args = {
     'mask_type': 'fofsub',
     'mask_args': (myobj, ),
     'mask_kwargs': dict(),
-    'configfile': os.path.expanduser'~/code/simobj/simobj/configs/example.py'),
+    'configfile': os.path.expanduser('~/code/simobj/simobj/configs/example.py'),
     'simfiles_configfile': os.path.expanduser('~/code/simfiles/simfiles/configs/example.py'),
     'cache_prefix': './',
     'disable_cache': False
