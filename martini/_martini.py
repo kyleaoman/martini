23e9227dad6e098e8950b9d5d63e3cdc62e9e9b4
K.A. Oman <koman@astro.rug.nl>
2017-11-20 14:11:01 +0100

    Remove timing information.

--- martini/_martini.py
+++ martini/_martini.py
@@ -61,30 +61,15 @@ class Martini():
             np.arange(self.datacube._array.shape[0]), 
             np.arange(self.datacube._array.shape[1])
         ))
-        import time
         for ij_px in ij_pxs:
             ij = np.array(ij_px)[..., np.newaxis] * U.pix
-            print(ij)
             mask = (ij - particle_coords[:2] <= sm_range).all(axis=0)
             weights = self.sph_kernel.line_integral(
                 np.power(particle_coords[:2, mask] - ij, 2).sum(axis=0), 
                 sm_length[mask]
             )
-            t0 = time.clock()
-            self.spectral_model.spectra[mask]
-            t1 = time.clock()
-            (self.spectral_model.spectra[mask] * weights[..., np.newaxis])
-            t2 = time.clock()
-            (self.spectral_model.spectra[mask] * weights[..., np.newaxis])\
-                .sum(axis=-2)
-            t3 = time.clock()
             (self.spectral_model.spectra[mask] * weights[..., np.newaxis])\
                 .sum(axis=-2, out=self.datacube._array[ij_px[0], ij_px[1], :, 0])
-            t4 = time.clock()
-            print('{:.6f}'.format(t1 - t0))
-            print('{:.6f}'.format(t2 - t1))
-            print('{:.6f}'.format(t3 - t2))
-            print('{:.6f}'.format(t4 - t3))
         
         return
 
