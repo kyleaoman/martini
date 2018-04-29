00868a0f59a02dbae98cbe8ec8fd8bc39283c1d2
Kyle Oman <koman@astro.rug.nl>
2018-04-29 09:47:01 +0200

    Docstrings for noise.

--- martini/noise.py
+++ martini/noise.py
@@ -4,6 +4,16 @@ import astropy.units as U
 import numpy as np
 
 class _BaseNoise(object):
+    """
+    Abstract base class to inherit for classes implementing a noise model.
+
+    Classes inheriting from _BaseNoise must implement a method 'generate' which receives one argument, a martini.DataCube instance. This method should return an astropy.units.Quantity array with the same shape as the datacube array, and units of Jy (or compatible).
+
+    See Also
+    --------
+    GaussianNoise (simple example implementing a Gaussian noise model)
+    """
+
     __metaclass__ = ABCMeta
     
     def __init__(self):
@@ -11,9 +21,22 @@ class _BaseNoise(object):
 
     @abstractmethod
     def generate(self, datacube):
+        """
+        Abstract method; create a cube containing noise.
+        """
         pass
 
 class GaussianNoise(_BaseNoise):
+    """
+    Implementation of a simple Gaussian noise model.
+    
+    Provides a 'generate' method producing a cube of Gaussian Noise.
+    
+    Parameters
+    ----------
+    rms : astropy.units.Quantity with dimensions of flux density (e.g. Jy)
+        Root mean square amplitude of the noise field.
+    """
     
     def __init__(self, rms=1.0 * U.Jy):
         
@@ -24,4 +47,15 @@ class GaussianNoise(_BaseNoise):
         return
 
     def generate(self, datacube):
+        """
+        Create a cube containing Gaussian noise.
+
+        Some numpy functions such as np.random.normal strip units, so need to handle them explicitly.
+        
+        Parameters
+        ----------
+        datacube : martini.DataCube instance
+            This method will be called passing the martini.DataCube instance as an argument; its attributes can thus be accessed here. datacube._array.shape is particularly relevant.
+        """
+
         return np.random.normal(scale=self.rms.value, size=datacube._array.shape) * self.rms.unit
