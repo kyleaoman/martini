3ec2812f5e527419d7773ed173b9b9525b4bbdbd
K.A. Oman <koman@astro.rug.nl>
2018-12-14 14:10:48 +0100

    Module level doc made accessible via help.

--- martini/__init__.py
+++ martini/__init__.py
@@ -3,7 +3,7 @@ from ._datacube import DataCube
 from ._demo import demo
 from ._version import __version__
 
-"""
+__doc__ = """
 Mock APERTIF-like Radio Telescope Interferometry of the Neutral ISM
 
 MARTINI is a modular package for the creation of synthetic resolved HI line
@@ -18,11 +18,13 @@ to the Martini class. A mock observation is then constructed by calling a
 handful of functions to execute the desired steps in the mock-observing
 process.
 
-The package is functional and (an old version) has been used in a forthcoming
-paper (https://ui.adsabs.harvard.edu/#abs/2017arXiv170607478O/abstract). It is
-still undergoing active development and is not quite yet at the point of a
-stable release version. Things will change, bugs will happen. Any feedback is
-greatly appreciated.
+The package is functional and (an old version) has been used in:
+Oman, K. A., Marasco, A., Navarro, J. F., Frenk, C. S., Schaye, J., Benitez-
+Llambay, A. 2019, MNRAS, 482, 821.
+Stable releases are available via PyPI (`pip install astromartini`) and the
+numbered branches on github. The github master branch is under active
+development: things will change, bugs will happen. Any feedback is greatly
+appreciated.
 
 See the help for martini.Martini for an example script to configure MARTINI
 and create a datacube. This example can be run by doing:
@@ -31,8 +33,8 @@ from martini import demo
 demo()
 
 If your use of MARTINI leads to a publication, please acknowledge this and link
-to the github page, ideally specifying the version used (git commit ID).
+to the github page, ideally specifying the version used (git commit ID or
+version number).
 
 Suport available via koman@astro.rug.nl.
 """
-
