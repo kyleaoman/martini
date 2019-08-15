91d3f1cf1e9d7536c639c1d9656726bde4230f23
Kyle Oman <koman@astro.rug.nl>
2019-08-15 15:12:34 +0200

    Add EAGLESource module and support for peculiar velocity.

--- martini/sources.py
+++ martini/sources.py
@@ -1,4 +1,5 @@
 import numpy as np
+from os.path import join
 from astropy.coordinates import CartesianRepresentation,\
     CartesianDifferential, ICRS
 from astropy.coordinates.matrix_utilities import rotation_matrix
@@ -27,6 +28,9 @@ class SPHSource(object):
     ----------
     distance : astropy.units.Quantity, with dimensions of length
         Source distance, also used to set the velocity offset via Hubble's law.
+
+    vpeculiar : astropy.units.Quantity, with dimensions of velocity
+        Source peculiar velocity, added to the velocity from Hubble's law.
 
     rotation : dict
         Keys may be any combination of 'axis_angle', 'rotmat' and/or
@@ -90,6 +94,7 @@ class SPHSource(object):
     def __init__(
             self,
             distance=3. * U.Mpc,
+            vpeculiar=0. * U.km / U.s,
             rotation={'rotmat': np.eye(3)},
             ra=0.*U.deg,
             dec=0.*U.deg,
@@ -136,21 +141,24 @@ class SPHSource(object):
         self.ra = ra
         self.dec = dec
         self.distance = distance
+        self.vpeculiar = vpeculiar
         self.rotation = rotation
         self.current_rotation = np.eye(3)
         self.rotate(**self.rotation)
         self.rotate(axis_angle=('y', self.dec))
         self.rotate(axis_angle=('z', -self.ra))
-        distance_vector = np.array([
+        direction_vector = np.array([
             np.cos(self.ra) * np.cos(self.dec),
             np.sin(self.ra) * np.cos(self.dec),
             np.sin(self.dec)
-        ]) * self.distance
+        ])
+        distance_vector = direction_vector * self.distance
         self.translate_position(distance_vector)
         self.vsys = \
             (self.h * 100.0 * U.km * U.s ** -1 * U.Mpc ** - 1) * self.distance
-        hubble_flow_vector = distance_vector * self.vsys / self.distance
-        self.translate_velocity(hubble_flow_vector)
+        hubble_flow_vector = direction_vector * self.vsys
+        vpeculiar_vector = direction_vector * self.vpeculiar
+        self.translate_velocity(hubble_flow_vector + vpeculiar_vector)
         self.sky_coordinates = ICRS(self.coordinates_g)
         return
 
@@ -289,6 +297,9 @@ class SingleParticleSource(SPHSource):
         Source distance, also used to place the source in the Hubble flow
         assuming h = 0.7.
 
+    vpeculiar : astropy.units.Quantity, with dimensions of velocity
+        Source peculiar velocity, added to the velocity from Hubble's law.
+
     ra : astropy.units.Quantity, with dimensions of angle
         Right ascension for the source centroid.
 
@@ -301,10 +312,17 @@ class SingleParticleSource(SPHSource):
         An appropriately initialized SingleParticleSource object.
     """
 
-    def __init__(self, distance=3 * U.Mpc, ra=0. * U.deg, dec=0. * U.deg):
+    def __init__(
+            self,
+            distance=3 * U.Mpc,
+            vpeculiar=0 * U.km / U.s,
+            ra=0. * U.deg,
+            dec=0. * U.deg
+    ):
 
         super().__init__(
             distance=distance,
+            vpeculiar=vpeculiar,
             rotation={'rotmat': np.eye(3)},
             ra=ra,
             dec=dec,
@@ -342,6 +360,9 @@ class CrossSource(SPHSource):
         Source distance, also used to place the source in the Hubble flow
         assuming h = 0.7.
 
+    vpeculiar : astropy.units.Quantity, with dimensions of velocity
+        Source peculiar velocity, added to the velocity from Hubble's law.
+
     rotation : dict
         Keys may be any combination of 'axis_angle', 'rotmat' and/or
         'L_coords'. These will be applied in this order. Note that the 'y-z'
@@ -375,6 +396,7 @@ class CrossSource(SPHSource):
     def __init__(
             self,
             distance=3. * U.Mpc,
+            vpeculiar=0 * U.km / U.s,
             rotation={'rotmat': np.eye(3)},
             ra=0. * U.deg,
             dec=0. * U.deg
@@ -392,6 +414,7 @@ class CrossSource(SPHSource):
 
         super().__init__(
             distance=distance,
+            vpeculiar=vpeculiar,
             rotation={'rotmat': np.eye(3)},
             ra=ra,
             dec=dec,
@@ -418,6 +441,9 @@ class SOSource(SPHSource):
     ----------
     distance : astropy.units.Quantity, with dimensions of length
         Source distance, also used to set the velocity offset via Hubble's law.
+
+    vpeculiar : astropy.units.Quantity, with dimensions of velocity
+        Source peculiar velocity, added to the velocity from Hubble's law.
 
     rotation : dict
         Keys may be any combination of 'axis_angle', 'rotmat' and/or
@@ -461,6 +487,7 @@ class SOSource(SPHSource):
     def __init__(
             self,
             distance=3.*U.Mpc,
+            vpeculiar=0*U.km/U.s,
             rotation={'L_coords': (60.*U.deg, 0.*U.deg)},
             ra=0.*U.deg,
             dec=0.*U.deg,
@@ -491,6 +518,7 @@ class SOSource(SPHSource):
         elif SO_instance is not None:
             super().__init__(
                 distance=distance,
+                vpeculiar=vpeculiar,
                 rotation=rotation,
                 ra=ra,
                 dec=dec,
@@ -537,6 +565,9 @@ class TNGSource(SPHSource):
     distance : astropy.units.Quantity, with dimensions of length
         Source distance, also used to set the velocity offset via Hubble's law.
 
+    vpeculiar : astropy.units.Quantity, with dimensions of velocity
+        Source peculiar velocity, added to the velocity from Hubble's law.
+
     rotation : dict
         Keys may be any combination of 'axis_angle', 'rotmat' and/or
         'L_coords'. These will be applied in this order. Note that the 'y-z'
@@ -572,6 +603,7 @@ class TNGSource(SPHSource):
             snapNum,
             subID,
             distance=3.*U.Mpc,
+            vpeculiar=0*U.km/U.s,
             rotation={'L_coords': (60.*U.deg, 0.*U.deg)},
             ra=0.*U.deg,
             dec=0.*U.deg
@@ -659,6 +691,7 @@ class TNGSource(SPHSource):
 
         super().__init__(
             distance=distance,
+            vpeculiar=vpeculiar,
             rotation=rotation,
             ra=ra,
             dec=dec,
@@ -668,5 +701,223 @@ class TNGSource(SPHSource):
             xyz_g=xyz_g,
             vxyz_g=vxyz_g,
             hsm_g=hsm_g
+        )
+        return
+
+
+class EAGLESource(SPHSource):
+    """
+    Class abstracting HI sources designed to work with publicly available
+    EAGLE snapshot + group files. For file access, see
+    http://icc.dur.ac.uk/Eagle/database.php.
+
+    Parameters
+    ----------
+    snapPath : string
+        Directory containing snapshot files.
+
+    snapBase : string
+        Filename of snapshot files, omitting portion '.X.hdf5'. Note these must
+        be the subfind-processed files, usually named 
+        'eagle_subfind_particles[...]'.
+
+    groupPath : string
+        Directory containing group catalog files.
+
+    groupBase : string
+        Filename of group catalog files, omitting portion '.X.hdf5'. Note
+        these must be the subfind tables, usually named 
+        'eagle_subfind_tab[...]'.
+
+    fof : int
+        FOF group number of the target object. Note that all particles in the
+        FOF group to which the subhalo belongs are used to construct the data
+        cube. This avoids strange "holes" at the locations of other subhaloes
+        in the same group, and gives a more realistic treatment of foreground
+        and background emission local to the source. In the EAGLE database,
+        this is the 'GroupNumber'.
+
+    sub : int
+        Subfind subhalo number of the target object. For centrals the subhalo
+        number is 0, for satellites >0. In the EAGLE database, this is then
+        'SubGroupNumber'.
+
+    subBoxSize : astropy.units.Quantity, with dimensions of length
+        Box half-side length of a region to load around the object of interest,
+        in physical (not comoving, no little h) units. This is only to avoid
+        needing to load the entire particle arrays. By default set to 1 Mpc,
+        which should be adequate for most galaxies.
+
+    distance : astropy.units.Quantity, with dimensions of length
+        Source distance, also used to set the velocity offset via Hubble's law.
+
+    vpeculiar : astropy.units.Quantity, with dimensions of velocity
+        Source peculiar velocity, added to the velocity from Hubble's law.
+
+    rotation : dict
+        Keys may be any combination of 'axis_angle', 'rotmat' and/or
+        'L_coords'. These will be applied in this order. Note that the 'y-z'
+        plane will be the one eventually placed in the plane of the "sky". The
+        corresponding values:
+        - 'axis_angle' : 2-tuple, first element one of 'x', 'y', 'z' for the \
+        axis to rotate about, second element an astropy.units.Quantity with \
+        dimensions of angle, indicating the angle to rotate through.
+        - 'rotmat' : A (3, 3) numpy.array specifying a rotation.
+        - 'L_coords' : A 2-tuple containing an inclination and an azimuthal \
+        angle (both astropy.units.Quantity instances with dimensions of \
+        angle). The routine will first attempt to identify a preferred plane \
+        based on the angular momenta of the central 1/3 of particles in the \
+        source. This plane will then be rotated to lie in the plane of the \
+        "sky" ('y-z'), rotated by the azimuthal angle about its angular \
+        momentum pole (rotation about 'x'), and inclined (rotation about 'y').
+
+    ra : astropy.units.Quantity, with dimensions of angle
+        Right ascension for the source centroid.
+
+    dec : astropy.units.Quantity, with dimensions of angle
+        Declination for the source centroid.
+
+    Returns
+    -------
+    out : EAGLESource
+        An appropriately initialized EAGLESource object.
+    """
+
+    def __init__(
+            self,
+            snapPath=None,
+            snapBase=None,
+            groupPath=None,
+            groupBase=None,
+            fof=None,
+            sub=None,
+            subBoxSize=1*U.Mpc,
+            distance=3.*U.Mpc,
+            vpeculiar=0*U.km/U.s,
+            rotation={'L_coords': (60.*U.deg, 0.*U.deg)},
+            ra=0.*U.deg,
+            dec=0.*U.deg
+    ):
+
+        if snapPath is None:
+            raise ValueError('Provide snapPath argument to EAGLESource.')
+        if snapBase is None:
+            raise ValueError('Provide snapBase argument to EAGLESource.')
+        if groupPath is None:
+            raise ValueError('Provide groupPath argument to EAGLESource.')
+        if groupBase is None:
+            raise ValueError('Provide groupBase argument to EAGLESource.')
+        if fof is None:
+            raise ValueError('Provide fof argument to EAGLESource.')
+        if sub is None:
+            raise ValueError('Provide sub argument to EAGLESource.')
+
+        # optional dependencies for this source class
+        from read_eagle import EagleSnapshot
+        from Hdecompose.atomic_frac import atomic_frac
+        import h5py
+
+        fileN = 0
+        while True:  # will break when group found or raise when out of files
+            groupFile = join(groupPath, groupBase+'.{:d}.hdf5'.format(fileN))
+            with h5py.File(groupFile, 'r') as g:
+                mask = np.logical_and(
+                    np.array(g['/Subhalo/GroupNumber']) == fof,
+                    np.array(g['/Subhalo/SubGroupNumber']) == sub
+                )
+                if not np.sum(mask):
+                    fileN += 1
+                    continue  # group not in this file, try next one
+                a = g['Header'].attrs['Time']
+                redshift = 1 / a - 1
+                h = g['Header'].attrs['HubbleParam']
+                lbox = g['Header'].attrs['BoxSize'] * U.Mpc / h
+                code_to_g = g['/Units'].attrs['UnitMass_in_g'] * U.g
+                code_to_cm = g['/Units'].attrs['UnitLength_in_cm'] * U.cm
+                code_to_cm_s = g['/Units'].attrs['UnitVelocity_in_cm_per_s'] \
+                    * U.cm / U.s
+                dset = g['/Subhalo/CentreOfPotential']
+                aexp = dset.attrs.get('aexp-scale-exponent')
+                hexp = dset.attrs.get('h-scale-exponent')
+                cop = (dset[mask, :] * np.power(a, aexp) * np.power(h, hexp)
+                       * code_to_cm).to(U.kpc)
+                dset = g['/Subhalo/Velocity']
+                aexp = dset.attrs.get('aexp-scale-exponent')
+                hexp = dset.attrs.get('h-scale-exponent')
+                vcent = (dset[mask, :] * np.power(a, aexp) * np.power(h, hexp)
+                         * code_to_cm_s).to(U.km / U.s)
+                break
+
+        snapFile = join(snapPath, snapBase+'.0.hdf5')
+        subBoxSize = (subBoxSize * h / a).to(U.Mpc).value
+        centre = (cop * h / a).to(U.Mpc).value
+        eagle_data = EagleSnapshot(snapFile)
+        region = np.vstack((
+            centre - subBoxSize,
+            centre + subBoxSize
+        )).T.flatten()
+        eagle_data.select_region(*region)
+
+        with h5py.File(snapFile, 'r') as f:
+
+            fH = f['/RuntimePars'].attrs['InitAbundance_Hydrogen']
+            fHe = f['/RuntimePars'].attrs['InitAbundance_Helium']
+            proton_mass = f['/Constants'].attrs['PROTONMASS'] * U.g
+            mu = 1 / (fH + .25 * fHe)
+            gamma = f['/RuntimePars'].attrs['EOS_Jeans_GammaEffective']
+            T0 = f['/RuntimePars'].attrs['EOS_Jeans_TempNorm_K'] * U.K
+
+            def fetch(att, ptype=0):
+                # gas is type 0, only need gas properties
+                tmp = eagle_data.read_dataset(ptype, att)
+                dset = f['/PartType{:d}/{:s}'.format(ptype, att)]
+                aexp = dset.attrs.get('aexp-scale-exponent')
+                hexp = dset.attrs.get('h-scale-exponent')
+                return np.array(tmp, dtype='f8') * np.power(a, aexp) \
+                    * np.power(h, hexp)
+
+            ng_g = fetch('GroupNumber')
+            particles = dict(
+                xyz_g=(fetch('Coordinates') * code_to_cm).to(U.kpc),
+                vxyz_g=(fetch('Velocity') * code_to_cm_s).to(U.km / U.s),
+                T_g=fetch('Temperature') * U.K,
+                hsm_g=(fetch('SmoothingLength') * code_to_cm).to(U.kpc)
+            )
+            rho_g = fetch('Density') * U.g * U.cm ** -3
+            SFR_g = fetch('StarFormationRate')
+            Habundance_g = fetch('ElementAbundance/Hydrogen')
+
+        particles['mHI_g'] = (atomic_frac(
+            redshift,
+            rho_g * Habundance_g / (mu * proton_mass),
+            particles['T_g'],
+            rho_g,
+            Habundance_g,
+            onlyA1=True,
+            EAGLE_corrections=True,
+            SFR=SFR_g,
+            mu=mu,
+            gamma=gamma,
+            fH=fH,
+            T0=T0
+        ) * code_to_g).to(U.solMass)
+
+        mask = ng_g == fof
+        for k, v in particles.items():
+            particles[k] = v[mask]
+
+        particles['xyz_g'] -= cop
+        particles['xyz_g'][particles['xyz_g'] > lbox / 2.] -= lbox.to(U.kpc)
+        particles['xyz_g'][particles['xyz_g'] < -lbox / 2.] += lbox.to(U.kpc)
+        particles['vxyz_g'] -= vcent
+
+        super().__init__(
+            distance=distance,
+            vpeculiar=vpeculiar,
+            rotation=rotation,
+            ra=ra,
+            dec=dec,
+            h=h,
+            **particles
         )
         return
