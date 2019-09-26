a29ba06bf06a750264040ca76efdcf9de034e75c
Kyle Oman <koman@astro.rug.nl>
2019-09-26 14:32:01 +0200

    Partial documetation & cleanup of magneticum_source

--- martini/_sources/_magneticum_source.py
+++ martini/_sources/_magneticum_source.py
@@ -5,13 +5,85 @@ from g3t.stable.g3read import GadgetFile, read_particles_in_box
 
 
 class MagneticumSource(SPHSource):
+    """
+    Class abstracting HI sources designed to work with Magneticum snapshot 
+    + group fies.
 
+    Parameters
+    ----------
+    snapBase : string
+        x
+
+    haloPosition : numpy.ndarray with shape (3, )
+        x
+
+    haloVelocity : numpy.ndarray with shape (3, )
+        x
+
+    haloRadius : float
+        x
+
+    groupFile : string
+        x
+
+    haloID : int
+        x
+
+    subhaloID : int
+        x
+
+    xH : float
+        x
+
+    Lbox : astropy.units.Quantity, with dimensions of length
+        x
+
+    internal_units : dict
+        x
+
+    distance : astropy.units.Quantity, with dimensions of length
+        Source distance, also used to set the velocity offset via Hubble's law.
+
+    vpeculiar : astropy.units.Quantity, with dimensions of velocity
+        Source peculiar velocity, added to the velocity from Hubble's law.
+
+    rotation : dict
+        Keys may be any combination of 'axis_angle', 'rotmat' and/or
+        'L_coords'. These will be applied in this order. Note that the 'y-z'
+        pane will be the one eventually placed in the plane of the "sky". The
+        corresponding values:
+        - 'axis_angle' : 2-tuple, first element one of 'x', 'y', 'z' for the \
+        axis to rotate about, second element an astropy.units.Quantity with \
+        dimensions of angle, indicating the angle to rotate through.
+        - 'rotmat' : A (3, 3) numpy.array specifying a rotation.
+        - 'L_coords' : A 2-tuple containing an inclination and an azimuthal \
+        angle (both astropy.units.Quantity instances with dimensions of \
+        angle). The routine will first attempt to identify a preferred plane \
+        based on the angular momenta of the central 1/3 of particles in the \
+        source. This plane wil then be rotated to lie in the plane of the \
+        "sky" ('y-z'), rotated by the azimuthal angle about its angular \
+        momentum pole (rotation about 'x'), and inclined (rotation about 'y').
+
+    ra : astropy.units.Quantity, with dimensions of angle
+        Right ascension for the source centroid.
+
+    dec : astropy.units.Quantity, with dimensions of angle 
+        Declination for the source centroid.
+
+    Returns
+    -------
+    out : MagneticumSource
+        An appropriately initialized MagneticumSource object.
+    """
     def __init__(
             self,
             snapBase=None,
             haloPosition=None,
             haloVelocity=None,
             haloRadius=None,
+            groupFile=None,
+            haloID=None,
+            subhaloID=None,
             xH=0.76,  # not in header
             Lbox=100*U.Mpc,  # what is it, actually?
             internal_units=dict(
@@ -26,6 +98,34 @@ class MagneticumSource(SPHSource):
             ra=0*U.deg,
             dec=0*U.deg
     ):
+
+        # I guess I should allow rescaling of radius to get fore/background
+
+        if (haloID is not None) or (subhaloID is not None) \
+           or (groupFile is not None):
+            if (haloPosition is not None) or (haloVelocity is not None) \
+               or (haloRadius is not None):
+                raise
+        else:
+            if (haloID is not None) and (subhaloID is not None):
+                raise
+
+        if (haloID is not None) or (subhaloID is not None):
+            f = g3read.GadgetFile(groupFile)
+            data_sub = f.read_new(blocks=["SPOS", "SVEL", "GRNR"], ptypes=[1])
+            data_fof = f.read_new(blocks=["RVIR", "FSUB"], ptypes=[0])
+            xyz = data_sub["SPOS"]
+            vxyz = data_sub["SVEL"]
+            fsub = data_fof["FSUB"]
+            rvir = data_fof["RVIR"]
+            grnr = data_sub["GRNR"]
+            if subhaloID is None:
+                subhaloID = fsub[haloID]
+            if haloID is None:
+                haloID = grnr[subhaloID]
+            haloPosition = xyz[fsub[haloID]]
+            haloVelocity = vx[fsub[haloID]]
+            haloRadius = rvir[haloID]
 
         particles = {}
 
