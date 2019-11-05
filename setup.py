832460df9919ea9e6a2d6a260b7b3f520dc81050
K.A. Oman <koman@astro.rug.nl>
2019-11-05 15:38:29 +0000

    Add missing indent.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open(
     os.path.join(os.path.dirname(__file__), 'martini', 'VERSION')
 ) as version_file:
-version = version_file.read().strip()
+    version = version_file.read().strip()
 
 setup(
     name='astromartini',
