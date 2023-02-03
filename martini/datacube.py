Fix sign error in freq channels, add datacube channel conversion tests.

martini/datacube.py
@@ -71,10 +71,10 @@ class DataCube(object):
         self.dec = dec
         self.padx = 0
         self.pady = 0
+        self._freq_channel_mode = False
         self._init_wcs()
         self._channel_mids()
         self._channel_edges()
-        self._freq_channel_mode = False
 
         return
 
@@ -167,7 +167,7 @@ class DataCube(object):
         def convert_to_Hz(q):
             return q.to(U.Hz, equivalencies=U.doppler_radio(HIfreq))
 
-        self.wcs.wcs.cdelt[2] = np.abs(
+        self.wcs.wcs.cdelt[2] = -np.abs(
             convert_to_Hz(self.wcs.wcs.cdelt[2] * self.units[2])
             - convert_to_Hz(0.0 * self.units[2])
         ).value
@@ -177,9 +177,9 @@ class DataCube(object):
         self.wcs.wcs.ctype[2] = "FREQ"
         self.units[2] = U.Hz
         self.wcs.wcs.cunit[2] = self.units[2].to_string("fits")
+        self._freq_channel_mode = True
         self._channel_mids()
         self._channel_edges()
-        self._freq_channel_mode = True
         return
 
     def velocity_channels(self):
@@ -202,9 +202,9 @@ class DataCube(object):
         self.wcs.wcs.ctype[2] = "VELO-OBS"
         self.units[2] = U.m * U.s**-1
         self.wcs.wcs.cunit[2] = self.units[2].to_string("fits")
+        self._freq_channel_mode = False
         self._channel_mids()
         self._channel_edges()
-        self._freq_channel_mode = False
         return
 
     def add_pad(self, pad):

tests/test_datacube.py
@@ -1,7 +1,10 @@
 import pytest
 import os
+import numpy as np
+from astropy import units as U, constants as C
 from astropy.units import isclose, allclose
 from martini import DataCube
+from martini.datacube import HIfreq
 
 
 class TestDataCube:
@@ -34,10 +37,31 @@ class TestDataCube:
         assert len(list(dc.spectra())) == dc.n_px_x * dc.n_px_y
 
     def test_freq_channels(self, dc):
-        raise NotImplementedError
+        v_mid0 = dc.channel_mids[0]
+        v_edge0 = dc.channel_edges[0]
+        v_mid1 = dc.channel_mids[-1]
+        v_edge1 = dc.channel_edges[-1]
+        f_mid0 = HIfreq * (1 - v_mid0 / C.c)
+        f_edge0 = HIfreq * (1 - v_edge0 / C.c)
+        f_mid1 = HIfreq * (1 - v_mid1 / C.c)
+        f_edge1 = HIfreq * (1 - v_edge1 / C.c)
+        dc.freq_channels()
+        assert allclose(dc.channel_mids, np.linspace(f_mid0, f_mid1, dc.n_channels), atol=1 * U.Hz)
+        assert allclose(dc.channel_edges, np.linspace(f_edge0, f_edge1, dc.n_channels + 1), atol=1 * U.Hz)
 
     def test_velocity_channels(self, dc):
-        raise NotImplementedError
+        dc.freq_channels()
+        f_mid0 = dc.channel_mids[0]
+        f_edge0 = dc.channel_edges[0]
+        f_mid1 = dc.channel_mids[-1]
+        f_edge1 = dc.channel_edges[-1]
+        v_mid0 = C.c * (1 - f_mid0 / HIfreq)
+        v_edge0 = C.c * (1 - f_edge0 / HIfreq)
+        v_mid1 = C.c * (1 - f_mid1 / HIfreq)
+        v_edge1 = C.c * (1 - f_edge1 / HIfreq)
+        dc.velocity_channels()
+        assert allclose(dc.channel_mids, np.linspace(v_mid0, v_mid1, dc.n_channels), atol=1e-3 * U.m / U.s)
+        assert allclose(dc.channel_edges, np.linspace(v_edge0, v_edge1, dc.n_channels + 1), atol=1e-3 * U.m / U.s)
 
     def test_channel_mode_switching(self, dc):
         initial_mids = dc.channel_mids