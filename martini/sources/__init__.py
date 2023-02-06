45017efad169a5ba552bbf778644ae264544225d
Kyle Oman <kyle.a.oman@durham.ac.uk>
2023-02-06 15:13:57 +0000

    Add ColibreSource to module listing.

--- martini/sources/__init__.py
+++ martini/sources/__init__.py
@@ -1,8 +1,16 @@
+# generic source class:
 from .sph_source import SPHSource
+
+# debugging source classes:
 from ._single_particle_source import _SingleParticleSource
 from ._cross_source import _CrossSource
-from .tng_source import TNGSource
+
+# simobj-specific source class:
+from .so_source import SOSource
+
+# simulation-specific source classes:
+from .colibre_source import ColibreSource
 from .eagle_source import EAGLESource
 from .magneticum_source import MagneticumSource
 from .simba_source import SimbaSource
-from .so_source import SOSource
+from .tng_source import TNGSource
