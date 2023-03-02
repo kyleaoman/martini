723f474c789adb59e5a29f311b9ab5a7f0437d3b
Kyle Oman <kyle.a.oman@durham.ac.uk>
2023-03-02 21:23:58 +0000

    Lint with black.

--- martini/beams.pyi
+++ martini/beams.pyi
@@ -6,7 +6,6 @@ import numpy as np
 from abc import abstractmethod
 from martini.datacube import DataCube as DataCube
 
-
 class _BaseBeam(metaclass=abc.ABCMeta):
     __metaclass__: Incomplete
     bmaj: U.Quantity[U.arcsec]
@@ -30,31 +29,20 @@ class _BaseBeam(metaclass=abc.ABCMeta):
         bmaj: U.Quantity[U.arcsec] = ...,
         bmin: U.Quantity[U.arcsec] = ...,
         bpa: U.Quantity[U.deg] = ...,
-    ) -> None:
-        ...
-
-    def needs_pad(self) -> T.Tuple[int, int]:
-        ...
+    ) -> None: ...
+    def needs_pad(self) -> T.Tuple[int, int]: ...
 
     vel: U.Quantity[U.km / U.s]
     ra: U.Quantity[U.deg]
     dec: U.Quantity[U.deg]
 
-    def init_kernel(self, datacube: DataCube) -> None:
-        ...
-
+    def init_kernel(self, datacube: DataCube) -> None: ...
     @abstractmethod
-    def f_kernel(self):
-        ...
-
+    def f_kernel(self): ...
     @abstractmethod
-    def kernel_size_px(self):
-        ...
-
+    def kernel_size_px(self): ...
     @abstractmethod
-    def init_beam_header(self):
-        ...
-
+    def init_beam_header(self): ...
 
 class GaussianBeam(_BaseBeam, metaclass=abc.ABCMeta):
     truncate: float
@@ -65,15 +53,10 @@ class GaussianBeam(_BaseBeam, metaclass=abc.ABCMeta):
         bmin: U.Quantity[U.arcsec] = ...,
         bpa: U.Quantity[U.deg] = ...,
         truncate: float = ...,
-    ) -> None:
-        ...
-
+    ) -> None: ...
     def f_kernel(
         self,
     ) -> T.Callable[
         [T.Union[float, np.ndarray], T.Union[float, np.ndarray]], U.Quantity
-    ]:
-        ...
-
-    def kernel_size_px(self) -> T.Tuple[int, int]:
-        ...
+    ]: ...
+    def kernel_size_px(self) -> T.Tuple[int, int]: ...
