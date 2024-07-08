fa35c33c78b8437e2ead8f4cdf392f23550fef8e
Kyle Oman <kyle.a.oman@durham.ac.uk>
2024-07-08 08:09:02 +0100

    Testing environment on github CI.

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -614,6 +614,9 @@ class TestTNGSource:
         from nbmake.pytest_items import NotebookFailedException
         import pathlib
 
+        print(os.environ)
+        print("VAR1", os.environ["VAR1"])
+        print("TNG_API_KEY", os.environ["TNG_API_KEY"])
         assert "TNG_API_KEY" in os.environ or os.path.isfile("examples/tng_api.key")
 
         if os.path.isfile("examples/martini-cutout-grnr-TNG100-1-99-400547.npy"):
